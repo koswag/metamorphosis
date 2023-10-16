0cbad46380ab202fd705bb3c1c48720d6d61f133
kskarzynski <k.skarzynski@sages.pl>
2023-10-16 17:59:22 +0200

    Mapper builder interface

---
+++ src/main/kotlin/mapper/builder/MapperBuilder.kt
@@ -0,0 +1,41 @@
+package mapper.builder
+
+import kotlin.reflect.KProperty1
+
+interface MapperBuilder<Source : Any, Target : Any> {
+
+    companion object {
+        data class TransformationBuilder<Source : Any, SourceProp, Target : Any, TargetProp>(
+            val sourceProp: KProperty1<Source, SourceProp>,
+            val targetProp: KProperty1<Target, TargetProp>,
+        )
+    }
+
+    fun <Prop> mapping(
+        sourceProp: KProperty1<Source, Prop>,
+        targetProp: KProperty1<Target, Prop>,
+    )
+
+    fun <SourceProp, TargetProp> transformation(
+        sourceProp: KProperty1<Source, SourceProp>,
+        targetProp: KProperty1<Target, TargetProp>,
+        transform: (SourceProp) -> TargetProp,
+    )
+
+
+    infix fun <Prop> KProperty1<Source, Prop>.mappedTo(targetProp: KProperty1<Target, Prop>) {
+        mapping(this, targetProp)
+    }
+
+    infix fun <SourceProp, TargetProp> KProperty1<Source, SourceProp>.transformedTo(
+        targetProp: KProperty1<Target, TargetProp>,
+    ): TransformationBuilder<Source, SourceProp, Target, TargetProp> =
+        TransformationBuilder(this, targetProp)
+
+    infix fun <SourceProp, TargetProp> TransformationBuilder<Source, SourceProp, Target, TargetProp>.using(
+        transform: (SourceProp) -> TargetProp,
+    ) {
+        transformation(sourceProp, targetProp, transform)
+    }
+
+}
