cbe18cfdcf039577c3f5aa387b6c0af0f2259d6d
kskarzynski <k.skarzynski@sages.pl>
2023-10-17 22:11:06 +0200

    Catching factory method

--- src/main/kotlin/com/github/koswag/morph/Mapper.kt
+++ src/main/kotlin/com/github/koswag/morph/Mapper.kt
@@ -12,6 +12,11 @@ interface Mapper<Source : Any, Target : Any> : (Source) -> Target {
         map(source)
 
     companion object {
+        inline fun <reified Source : Any, reified Target : Any> buildCatching(
+            noinline mappingBlock: MapperBuilder<Source, Target>.() -> Unit,
+        ): Result<Mapper<Source, Target>> =
+            runCatching { Mapper(mappingBlock) }
+
         inline operator fun <reified Source : Any, reified Target : Any> invoke(
             noinline mappingBlock: MapperBuilder<Source, Target>.() -> Unit,
         ): Mapper<Source, Target> =
